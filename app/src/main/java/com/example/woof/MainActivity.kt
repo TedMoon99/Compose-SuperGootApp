ea16bf90826d2ee5fe79a7f3a37218620f961801
TedMoon99 <mtj1015@naver.com>
2024-11-01 20:46:52 +0900

    Jetpack Compose를 사용한 간단한 애니메이션

--- app/src/main/java/com/example/woof/MainActivity.kt
+++ app/src/main/java/com/example/woof/MainActivity.kt
@@ -22,12 +22,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -35,14 +39,23 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -127,16 +140,93 @@ fun DogItem(
     dog: Dog,
     modifier: Modifier = Modifier
 ) {
+
+    var expanded by remember { mutableStateOf(false) }
+
     Card(modifier= modifier) {
-        Row(
-            modifier = modifier
-                .fillMaxWidth()
-                .padding(dimensionResource(R.dimen.padding_small))
+        Column(
+            // Animation 적용
+            modifier = Modifier.animateContentSize(
+                // animationSpec을 사용하여 기기별로 맞춤 설정할 수 있다
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioHighBouncy, // dampingRatio: 감쇠비. 물체의 운동을 저지시키려는 단위 속도당 힘
+                    stiffness = Spring.StiffnessVeryLow, // stiffness: 강성. 변형에 저항하는 성질
+                )
+            )
         ) {
-            DogIcon(dog.imageResourceId)
-            DogInformation(dog.name, dog.age)
+            Row(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(dimensionResource(R.dimen.padding_small))
+            ) {
+                DogIcon(dog.imageResourceId)
+                DogInformation(dog.name, dog.age)
+                // Spacer가 Row에서 유일하게 가중치가 적용됨
+                // 가중치가 없는 다른 요소의 너비를 측정한 후에 남은 공간을 채워준다
+                Spacer(modifier = Modifier.weight(1f))
+                DogItemButton(
+                    expanded = expanded,
+                    onClick = {
+                        /* 클릭 시 위의 expanded 값을 바꿔준다 */
+                        expanded = !expanded
+                    },
+                )
+            }
+
+            // DogHobby
+            if (expanded) {
+                DogHobby(
+                    dog.hobbies,
+                    modifier= Modifier.padding(
+                        start= dimensionResource(R.dimen.padding_medium),
+                        top =  dimensionResource(R.dimen.padding_small),
+                        end =  dimensionResource(R.dimen.padding_medium),
+                        bottom = dimensionResource(R.dimen.padding_medium)
+                    )
+                )
+            }
         }
     }
+}
+
+@Composable
+fun DogItemButton(
+    expanded: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    IconButton(
+        onClick= onClick,
+        modifier = modifier,
+    ) {
+        Icon(
+            imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+            contentDescription = stringResource(R.string.expand_button_content_description),
+            tint = MaterialTheme.colorScheme.secondary, // 보조색 적용
+        )
+    }
+}
+
+@Composable
+fun DogHobby(
+    @StringRes dogHobby: Int,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier= modifier
+    ) {
+        Text(
+            text =  stringResource(R.string.about),
+            style = MaterialTheme.typography.labelSmall
+        )
+
+        Text(
+            text =  stringResource(dogHobby),
+            style = MaterialTheme.typography.bodyLarge
+        )
+
+    }
+
 }
 
 /**
